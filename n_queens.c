aee686f920d102a8e3c6277490da45f5e3e9366c
Oussama Elhassouni <oelhasso@c1r6p6.1337.ma>
2025-04-20 22:56:39 +0100

    still a small problem

--- n_queens.c
+++ n_queens.c
@@ -164,156 +164,142 @@ void set_up(int head, int *range, int nb, int *sol)
 	int i = 0;
 	while (i < nb)
 	{
-		myputstr("while, i = ");
-		myputnbr(i);
-		myputstr(", head = ");
-		myputnbr(head);
-		myputstr("\n");
+		// myputstr("while, i = ");
+		// myputnbr(i);
+		// myputstr(", head = ");
+		// myputnbr(head);
+		// myputstr("\n");
 		if (head == 0)
 		{
-			myputstr("head is 0\n");
-			head++;
-			range[0] = head;
-			i++;
-			set_up(head, range, nb, sol);
-			myputstr("lhna\n");
+			// myputstr("head is 0\n");
+			range[head] = i;
+			set_up(head + 1, range, nb, sol);
+			// myputstr("lhna\n");
 			continue;
 		}
-		if (sameline(range, head, i) == 1)
+		else
 		{
-			myputstr("same line\n");
-			myputstr("i++\n");
-			i++;
-			continue;
+			if (sameline(range, head, i) == 1)
+			{
+				// myputstr("same line\n");
+				// myputstr("i++\n");
+				i++;
+				continue;
+			}
+			else if (x_dire(range, nb, head, i))
+			{
+				// myputstr("x_dire\n");
+				// myputstr("i++\n");
+				i++;
+				continue;
+			}
+			// myputstr("set range\n");
+			range[head] = i;
+			if (head == nb - 1)
+			{
+				// myputstr("write\n");
+				writeit(sol, range, nb);
+				i++;
+				continue;
+			}
+			// myputstr("go again\n");
+			set_up(head + 1, range, nb, sol);
 		}
-		else if (x_dire(range, nb, head, i))
-		{
-			myputstr("x_dire\n");
-			myputstr("i++\n");
-			i++;
-			continue;
-		}
-		myputstr("set range\n");
-		range[head] = i;
-		if (head == nb - 1)
-		{
-			myputstr("write\n");
-			writeit(sol, range, nb);
-			i++;
-			continue;
-		}
-		head++;
-		myputstr("go again\n");
-		set_up(head, range, nb, sol);
 		i++;
 	}
-	myputstr("raje rjae\n");
+	// myputstr("raje rjae\n");
 	return;
 }
 
-int teenqueens(char *str)
+void teenqueens(char *input)
 {
-	int sol;
+	int sol = 0;
 	int nb;
 	sol = 0;
-    nb = myatoi(str);
+    nb = myatoi(input);
 	if (nb < 0)
 	{
 		myputstr ("not a positive nb\n");
-		return -1;
+		return ;
 	}
 	if (nb < 4)
 	{
 		if (nb == 1)
 		{
 			myputstr ("1\n");
-			return 1;
+			myputstr ("solutions are : 1\n");
+			return ;
 		}
-		return 0;
+		myputstr ("no solution\n");
+		return ;
 	}
-	int i = 0;
-    int *range;
-	range = malloc (sizeof(int) * nb);
-	if (!range)
-	{
-		myputstr("range failed\n");
-		return 1;
-	}
+    int range[nb];
 	range[0] = -1;
 	set_up(0, range, nb, &sol);
-	return sol;
+	return ;
 }
 
-char *gnl(int fd)
+// char *gnl(int fd)
+// {
+//     char *str = NULL;
+//     char *readd = NULL;
+//     char *buffer = NULL;
+//     char *tmp = NULL;
+//     int i;
+//     int j;
+//     int r;
+//     int buffer_size = 1;
+//     while (1)
+//     {
+//         readd = malloc (buffer_size + 1);
+//         if (!readd)
+//         {
+//             myputstr ("readd allocation failed\n");
+//             return NULL;
+//         }
+//         r = read (fd, readd, buffer_size);
+//         if (r == -1)
+//         {
+//             myputstr ("r == -1\n");
+//             return NULL;
+//         }
+//         if (r == 0)
+//             break;
+//         readd[r] = 0;
+//         if (buffer)
+//             tmp = buffer;
+//         buffer = malloc (mystrlen(tmp) + mystrlen(readd) + 1);
+//         if (!buffer)
+//         {
+//             myputstr ("buffer alllcation failed \n");
+//             return NULL;
+//         }
+//         i = 0;
+//         if (tmp)
+//         {
+//             j = 0;
+//             while (tmp[j])
+//                 buffer[i++] = tmp[j++];
+//         }
+//         j = 0;
+//         while (readd[j])
+//             buffer[i++] = readd[j++];
+//         buffer[i] = 0;
+//         free(tmp);
+//         free(readd);
+//         if (full_line(buffer) == 1)
+//             return buffer;
+//     }
+//     if (!buffer && r == 0)
+//         return NULL;
+//     return buffer;
+// }
+int main (int ac, char **av)
 {
-    char *str = NULL;
-    char *readd = NULL;
-    char *buffer = NULL;
-    char *tmp = NULL;
-    int i;
-    int j;
-    int r;
-    int buffer_size = 1;
-    while (1)
-    {
-        readd = malloc (buffer_size + 1);
-        if (!readd)
-        {
-            myputstr ("readd allocation failed\n");
-            return NULL;
-        }
-        r = read (fd, readd, buffer_size);
-        if (r == -1)
-        {
-            myputstr ("r == -1\n");
-            return NULL;
-        }
-        if (r == 0)
-            break;
-        readd[r] = 0;
-        if (buffer)
-            tmp = buffer;
-        buffer = malloc (mystrlen(tmp) + mystrlen(readd) + 1);
-        if (!buffer)
-        {
-            myputstr ("buffer alllcation failed \n");
-            return NULL;
-        }
-        i = 0;
-        if (tmp)
-        {
-            j = 0;
-            while (tmp[j])
-                buffer[i++] = tmp[j++];
-        }
-        j = 0;
-        while (readd[j])
-            buffer[i++] = readd[j++];
-        buffer[i] = 0;
-        free(tmp);
-        free(readd);
-        if (full_line(buffer) == 1)
-            return buffer;
-    }
-    if (!buffer && r == 0)
-        return NULL;
-    return buffer;
-}
-int main ()
-{
-    char *str;
-	int solutions;
-    while (1)
-    {
-        str = gnl(0);
-        if (!str)
-            return 1;
-        solutions = teenqueens(str);
-		if (solutions == -1)
-			return 1;
-        myputstr("\nsolutions are : ");
-        myputnbr(solutions);
-        myputstr("\n");
-    }
-
+	if (ac != 2)
+	{
+		myputstr("the number of arguments isnt valid\n");
+		return 1;
+	}
+	teenqueens(av[1]);
 }
